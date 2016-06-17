6401fdcebf1c0d92e2bce9222022296d3a7a3ff2
Anatoly <teseyko@outlook.com>
2016-06-18 02:14:46 +0300

    Update product_search.rb

--- product_search.rb
+++ product_search.rb
@@ -4,53 +4,45 @@ require 'open-uri'
 require 'fileutils'
 require 'csv'
 
-BASE_DIR = 'output/'
+require_relative 'conf'
+
 Dir.mkdir(BASE_DIR) unless File.exists?(BASE_DIR)
 
-BASE_URL = 'https://www.viovet.co.uk'
-INPUT_URL = '/Hills_Pet_Nutrition_Dog_Food/c2678/'
-PAGINATION = '?_=X&page='
-CATEGORY_URL = "#{BASE_URL}#{INPUT_URL}"
+url_file = ARGV[0]
+csv_file_name = ARGV[1]
+input_url = File.open(url_file).read.split(BASE_URL)
+category_url = FULL_URL + input_url.last
 
-CSV_HEADER = %w(
-            Название
-            Весовка
-            Цена
-            Картинка
-            Доставка
-            Код
-            )
-CSV.open(BASE_DIR + 'output.csv', 'wb') do |csv|
+CSV.open(BASE_DIR + csv_file_name, 'wb') do |csv|
   csv << CSV_HEADER
-  get_page = Nokogiri::HTML(open (CATEGORY_URL)).at('.pagination')
-  last_page = get_page.css('a').last.attr('href')
-  last_page = last_page.slice(last_page.length - 1)
-
-  (1..last_page.to_i).each do |page|
-# Find links
-    get_link = Nokogiri::HTML(open (CATEGORY_URL + PAGINATION + page.to_s)).at('.families-list')
+  pagination = Nokogiri::HTML(open (category_url)).at('.pagination')
+  page_list = []
+  pagination.css('a').each do |pages|
+  page_list.push (pages.attr('href').match(/page=(\d+)/)[1].to_i)
+end
+(1..page_list.max).each do |page|
+  p 'Page: #' + page.to_s 
+  
+    get_link = Nokogiri::HTML(open (category_url + PAGINATION + page.to_s)).at('.families-list')
     get_link.css('a.call_to_action._secondary._medium.view_details').each do |link|
       link_list = link.attr('href')
-      uri = BASE_URL + link_list #our page-link
-      p 'Fetching: ' + uri
+      uri = FULL_URL + link_list
+      p 'Fetching: ' + link_list
 
-#Extract data from page
       page = Nokogiri::HTML(open(uri)).at('.content-box')
       product = page.at_css('#product_family_heading').inner_text
-      #dir = BASE_DIR
-      #Dir.mkdir(dir) unless File.exists?(dir)
-      image = page.at('._img_zoom').attr('src').delete("\t\n")
+      image = page.at('img').attr('src').delete("\t\n")
       page.search('li.product').each do |item|
         items = {
-            name: product,
-            filling: item.search('div.title')[0].text.delete("\t\n"),
-            price: item.search('div.ours span')[0].text.delete("\t\n"),
-            picture: image,
-            delivery: item.search('.in-stock').inner_text.delete("\t\n"),
-            code: item.at('a strong').text,
+              name: product + ' - ' + item.search('div.title')[0].text.delete("\t\n"),
+              price: item.search('div.ours span')[0].text.delete("\t\n"),
+              picture: image,
+              delivery: item.search('.in-stock').inner_text.delete("\t\n"),
+              code: item.at('a strong').text,
         }
         csv << items.values
       end # item
     end # link
   end # page
-end # cvs
+end # csv
+p 'Done.'
